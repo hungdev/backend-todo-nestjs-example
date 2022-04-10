87041dbd68d5b0730fc4a1b407dee6e3e86f7d2a
Cee <hungns126@gmail.com>
2022-04-10 12:18:54 +0700

    done crud

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TasksController } from './tasks/tasks.controller';
+import { TasksService } from './tasks/tasks.service';
+import { TasksModule } from './tasks/tasks.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [TasksModule],
 })
-export class AppModule {}
+export class AppModule { }
